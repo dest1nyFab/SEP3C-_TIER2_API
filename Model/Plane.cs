66460dfc73a764e461aacdfd4b8bc85c5f701a81
Fabian Bernhardt <fabian_stefan@rocketmail.com>
2019-12-15 11:41:13 +0100

    up Plane Position

--- Model/Plane.cs
+++ Model/Plane.cs
@@ -6,11 +6,11 @@
         public string Model { get; set; }
         public string Company { get; set; }
         public FlightPlan FlightPlan { get; set; }
-        public Position Position { get; set; }
+        public Position PlanePosition { get; set; }
         public string Status { get; set; }
         public override string ToString()
         {
-            return $"RegNo: {RegistrationNo} \nModel:{Model} \nCompany: {Company} \nFlightPlan: {FlightPlan} \nPosition: {Position} \nStatus: {Status}";
+            return $"RegNo: {RegistrationNo} \nModel:{Model} \nCompany: {Company} \nFlightPlan: {FlightPlan} \nPosition: {PlanePosition} \nStatus: {Status}";
         }
     }
 }
